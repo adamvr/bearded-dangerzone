b81c5ba5632735c98c941c294d1ee2d7ad31b99a
Adam Rudd <adam.rudd@uqconnect.edu.au>
2013-05-28 22:30:15 +0200

    Add subscription stuff to views

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,39 +1,40 @@
 var template = _.template(
-  '<span class="date"><%= new Date() %>' + 
+  '<span class="date"><%= new Date() %></span>' +
   '<span class="topic"><%= topic %></span>' +
-  '<span class="message"><%= message %></span>'
+  '<span class="message"><%= message %></span>' 
 );
 
+
 var setupClickHandler = function () {
-  var publishButton = document.querySelector('#publish');
+  var publishButton = document.querySelector('#publish')
+    , topic = document.querySelector('#pub-topic')
+    , message = document.querySelector('#message');
 
   publishButton.onclick = function () {
-    var topic = document.querySelector('#topic').value
-      , message = document.querySelector('#message').value;
-    window.socket.emit('publish', topic, message);
+    window.socket.emit('publish', topic.value, message.value);
   };
 };
 
 var setupSockets = function () {
-   var socket = io.connect()
-    , messages = document.querySelector('#messages');
+  var messages = document.querySelector('#message-list');
 
+  var socket = window.socket = io.connect();
   socket.on('connect', function () {
-    var status = document.querySelector('#status p');
+    var status = document.querySelector('span.status');
+    status.className = 'status online';
     status.innerText = 'Online';
 
-    socket.emit('subscribe', '/#');
+    socket.emit('subscribe', '#');
   });
 
   socket.on('message', function(topic, message) {
     console.log('message received: ', message);
-    var element = document.createElement('p');
-    element.innerHTML = template({topic: topic, message: message});
 
-    messages.insertBefore(element, messages.firstChild);
+    var m = document.createElement('p');
+
+    m.innerHTML = template({topic: topic, message: message});
+    messages.insertBefore(m, messages.firstChild);
   });
-
-  window.socket = socket;
 };
 
 window.onload = function() {
